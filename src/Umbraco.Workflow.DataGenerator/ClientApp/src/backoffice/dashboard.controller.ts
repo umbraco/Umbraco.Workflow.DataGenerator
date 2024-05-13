d39a5d20471b908c58a8bc2b72cf90e0167dc93e
Nathan Woulfe <nathan@nathanw.com.au>
2024-05-13 10:50:52 +1000

    fix localization, bump version

--- src/Umbraco.Workflow.DataGenerator/ClientApp/src/backoffice/dashboard.controller.ts
+++ src/Umbraco.Workflow.DataGenerator/ClientApp/src/backoffice/dashboard.controller.ts
@@ -38,7 +38,7 @@ export class GeneratorDashboardController {
 
   constructor(private wfGeneratorService, private tourService, private overlayService, private localizationService, private notificationsService) {
     this.baseProperties.forEach(async prop => {
-      const results = await this.localizationService.localizeMany([`workflowDataGenerator${prop.alias}`, `workflowDataGenerator${prop.alias}Description`]);
+      const results = await this.localizationService.localizeMany([`workflowDataGenerator_${prop.alias}`, `workflowDataGenerator_${prop.alias}Description`]);
       prop.label = results[0];
       if (results[1].startsWith('[')) return;
       prop.description = results[1];
@@ -73,7 +73,7 @@ export class GeneratorDashboardController {
   }
 
   async showNotificationOverlay() {
-    const [title, content] = [...(await this.localizationService.localizeMany(['workflowDataGenerator_postInstallTitle', 'workflowDataGenerato_rpostInstallContent']))];
+    const [title, content] = [...(await this.localizationService.localizeMany(['workflowDataGenerator_postInstallTitle', 'workflowDataGenerator_postInstallContent']))];
 
     const overlayModel = {
       title,
